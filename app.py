0b5ff918f4166ae0640731602e5625480ada3c10
SaadSidd29 <saad.sidd29@gmail.com>
2022-07-31 20:57:02 +0530

    ML Project Structure Created

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request,jsonify
-
+from flask import Flask, render_template, request, jsonify
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  # route to display the home page
-def homePage():
+@app.route('/',methods=['GET','POST'])  # route to display the home page
+def index():
+    try:
+        raise Exception("We are trying custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "CI CD Pipeline"
 
 if __name__ == "__main__":
